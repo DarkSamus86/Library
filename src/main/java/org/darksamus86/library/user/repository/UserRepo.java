bbfd5b29fc10ea3d7de9edb1006980b2dd61c2ff
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-04-15 17:37:06 +0500

    feat: in UserRepo added method and create jpa repo for Role entity.

--- src/main/java/org/darksamus86/library/user/repository/UserRepo.java
+++ src/main/java/org/darksamus86/library/user/repository/UserRepo.java
@@ -3,5 +3,11 @@ package org.darksamus86.library.user.repository;
 import org.darksamus86.library.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
