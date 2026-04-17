7826e490bc1ef95bce42b7f8302c69b27b3befa2
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-04-17 16:22:58 +0500

    fix: resolved bug about unsynch of seq in db.

--- src/main/resources/db/migration/V3__seed_users.sql
+++ src/main/resources/db/migration/V3__seed_users.sql
@@ -1,26 +1,25 @@
 -- =========================
 -- ROLES
 -- =========================
-insert into roles (id, name, description) values
-                                              (1, 'ROLE_USER', 'Default user role'),
-                                              (2, 'ROLE_ADMIN', 'Administrator role');
+insert into roles (name, description) values
+                                          ('ROLE_USER', 'Default user role'),
+                                          ('ROLE_ADMIN', 'Administrator role');
 
 -- =========================
 -- USERS
 -- =========================
--- password: 123456 (bcrypt)
--- hash сгенерирован заранее
+-- password: 123456 (bcrypt hash)
+-- id генерируется автоматически
 insert into users (
-    id, email, username, password_hash,
+    email, username, password_hash,
     first_name, last_name,
     is_active, is_email_verified,
     created_at, updated_at
 ) values
       (
-          1,
           'admin@library.com',
           'admin',
-          '$2a$10$7QJ5kQ1mYF1pYkZ6w0m8K.1j8G9wY6ZQeF0u8v8yJ8c6X3hKkP1lO',
+          '$2a$10$3mlp6cmwpN8fQJNpanON8.IEFx49Q4wjZJcJQZYsOMUVQT2irjt0S',
           'Admin',
           'User',
           true,
@@ -29,10 +28,9 @@ insert into users (
           now()
       ),
       (
-          2,
           'user@library.com',
           'user',
-          '$2a$10$7QJ5kQ1mYF1pYkZ6w0m8K.1j8G9wY6ZQeF0u8v8yJ8c6X3hKkP1lO',
+          '$2a$10$3mlp6cmwpN8fQJNpanON8.IEFx49Q4wjZJcJQZYsOMUVQT2irjt0S',
           'Regular',
           'User',
           true,
@@ -44,6 +42,7 @@ insert into users (
 -- =========================
 -- USER ROLES
 -- =========================
-insert into user_roles (id, user_id, role_id) values
-                                                  (1, 1, 2), -- admin → ROLE_ADMIN
-                                                  (2, 2, 1); -- user → ROLE_USER
+-- id генерируется автоматически, указываем только user_id и role_id
+insert into user_roles (user_id, role_id) values
+                                              (1, 2), -- admin → ROLE_ADMIN
+                                              (2, 1); -- user → ROLE_USER
