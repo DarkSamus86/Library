49b7a052109626a69d6e27b07a64a204b3fab1b6
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-04-24 16:20:14 +0500

    feat: implement swagger.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
 
     // === DATABASE & MIGRATION ===
     implementation("org.flywaydb:flyway-core")  // версия управляется Spring Boot
