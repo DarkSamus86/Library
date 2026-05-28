1e9d681da885fd1b7fc83dcebf9d9956227df5c6
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-05-28 20:57:21 +0500

    feat: impl new abstract method, and added pagination in user repo and book repo.

--- src/main/java/org/darksamus86/library/book/repository/BookRepo.java
+++ src/main/java/org/darksamus86/library/book/repository/BookRepo.java
@@ -8,12 +8,11 @@ import java.util.Optional;
 
 @Repository
 public interface BookRepo extends JpaRepository<Book, Long> {
-    // Проверка существования по ISBN
     boolean existsByIsbn(String isbn);
 
-    // Поиск по ISBN
     Optional<Book> findByIsbn(String isbn);
 
-    // Поиск по названию (частичное совпадение)
     java.util.List<Book> findByTitleContainingIgnoreCase(String title);
+
+    long countByIsActive(boolean isActive);
 }
