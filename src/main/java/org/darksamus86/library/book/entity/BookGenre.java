54479c65289ce5f15fd3931fe69c13316ba62df5
Daniil Lobanov <daniillobanov1486@gmail.com>
2026-07-15 16:50:27 +0500

    feat: implement new tables (genres, book_genres).

---
+++ src/main/java/org/darksamus86/library/book/entity/BookGenre.java
@@ -0,0 +1,26 @@
+package org.darksamus86.library.book.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "book_genres")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BookGenre {
+    @EmbeddedId
+    private BookGenreId id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("bookId")
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @MapsId("genreId")
+    @JoinColumn(name = "genre_id", nullable = false)
+    private Genre genre;
+}
---
+++ src/main/java/org/darksamus86/library/book/entity/Genre.java
@@ -0,0 +1,28 @@
+package org.darksamus86.library.book.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "genres")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
